386e5cfe852c275c64a226f738093dfa909b0539
Martin Jørgensen <mrtnrjgsn@gmail.com>
2023-06-06 17:35:32 +0200

    Functional messaging between client and server

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,5 @@
+
+
 use crate::game::*;
 
 #[test]
@@ -40,53 +42,64 @@ fn test_win_conditions() -> (){
 
 pub mod test_tcp
 {
-    use std::{net::{TcpListener, TcpStream}, io::{Write, Read}};
-    const PORT: &str = "6666";
-    const MARTIN: &str = "192.168.43.20";
-    const SIGURD: &str = "172.16.216.132";
-    const DESTINATION: &str = SIGURD;
-    const SOURCE: &str = MARTIN;
+    const SERVER_IP: &str = "192.168.43.20:7878";
     
     #[test]
-    fn test_tcp_ping_server()
+    fn test_tcp_message_from_user_server()
     {
-        let listener = loop
-        {
-            match TcpListener::bind(SOURCE.to_string() + ":" + PORT)
-            {
-                Ok(listener) => break listener,
-                Err(err) => ()
-            }
+        use std::{
+            net::{TcpListener, TcpStream},  
+            io::{Write, Error, prelude::*, BufReader},
+            thread,
         };
-        for stream in listener.incoming()
-        {
-            let mut stream = stream.expect("Invalid stream!");
+        let listener=  TcpListener::bind(SERVER_IP.to_string()).expect("Could not bind!");
     
-            let mut request_message = vec![];
-            stream.read(&mut request_message).expect("Unable to read");
-            stream.write_all(&[b"Ping test! all good! Received: {}".to_vec(), request_message].concat())
-                .expect("Unable to write");
-            
-            println!("Connection established!");
+        for stream in listener.incoming() {
+            match stream {
+                Err(e) => { eprintln!("failed: {}", e) }
+                Ok(stream) => {
+                    thread::spawn(move || {
+                        handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                    });
+                }
+            }
+        }
+
+        fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+            println!("Incloming connection from: {}", stream.peer_addr()?);
+            let mut buffer = vec![];
+            loop {
+                let mut reader = BufReader::new(&stream);
+                reader.read_until(b'\n', &mut buffer).expect("Could not read into buffer");
+
+                print!("{}", String::from_utf8(buffer.clone()).expect("Failed to parse buffer"));
+                stream.write(&buffer)?;
+            }
         }
     }
     
     #[test]
-    fn test_tcp_ping_client()
+    fn test_tcp_message_from_user_client()
     {    
-        let mut stream = TcpStream::connect(DESTINATION.to_string() + ":" + PORT)
-            .expect("Unable to connect!");
-        stream.write(b"Ping!").expect("Unable to write ping");
-    
-        let listener = TcpListener::bind(SOURCE.to_string() + ":" + PORT).expect("Unable to bind!");
-        for stream in listener.incoming()
-        {
-            let mut stream = stream.expect("Invalid stream!");
-    
-            let mut request_message = vec![];
-            stream.read(&mut request_message).expect("Unable to read");
-            
-            println!("Connection established! Received: {}", String::from_utf8(request_message).expect("Invalid string"));
+        use std::{
+            net::{TcpStream}, 
+            io::{self, BufRead, BufReader, Write},
+            str
+        };
+
+        // const SERVER_IP: &str = "192.168.43.20:7878";
+
+        let mut stream = TcpStream::connect(SERVER_IP.to_string()).expect("Could not connect to server!");
+        loop {
+            let mut input = String::new();
+            let mut buffer : Vec<u8> = Vec::new();
+            io::stdin().read_line(&mut input).expect("Failed to read from stdin");
+            stream.write(input.as_bytes()).expect("Failed to write to server");
+
+            let mut reader = BufReader::new(&stream);
+
+            reader.read_until(b'\n', &mut buffer).expect("Could not read into buffer");
+            print!("{}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
         }
     }
 }
