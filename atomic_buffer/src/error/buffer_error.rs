b09e3b25d3e7590f59a9f2a5257c24d2b1fff401
Sigurd Sætherø Spangelo <sigurd-s@online.no>
2024-02-04 01:02:42 +0100

    Use thiserror for atomic buffer

--- atomic_buffer/src/error/buffer_error.rs
+++ atomic_buffer/src/error/buffer_error.rs
@@ -1,10 +1,24 @@
-use poison_error_obj::{PoisonErrorUnguarded, PoisonErrorObj};
+use std::fmt::Display;
 
-#[derive(Debug)]
+use poison_error_obj::{PoisonErrorUnguarded, PoisonErrorObj};
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone, Copy)]
 pub enum BufferError
 {
     NullPointerError,
     PoisonError(PoisonErrorUnguarded),
+}
+impl Display for BufferError
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        match self
+        {
+            Self::NullPointerError => write!(f, "Buffer Error: Null Pointer Error"),
+            Self::PoisonError(error) => write!(f, "Buffer Error: {}", error)
+        }
+    }
 }
 impl<'a, T> From<T> for BufferError
 where
